1af51ba418202ca404af906261fac0d1693cc229
Keith Hansen <designsbykeith@gmail.com>
2014-12-29 14:07:06 -0800

    index updates

--- jsx/components/App.js
+++ jsx/components/App.js
@@ -5,7 +5,7 @@ var React = require('react');
 var App = React.createClass({
     render: function () {
         return (
-            <div>NodeJS/React/Flux</div>
+            <div>NodeJS.React.Flux</div>
         );
     }
 });
